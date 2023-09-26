c171096eef24b06d4b057aed88a469b6f988e10f
fabriccio27 <fabricio_inc27@hotmail.com>
2023-09-26 17:15:38 -0300

    Replace default app.component - add global styles from repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // this is the tag name that will be used in the index.html file
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// in the class I can define the properties and methods that will be used in the component
 export class AppComponent {
-  title = 'angular-crash';
+  // I can access this property in the HTML file using the interpolation syntax {{ title }}
+  // that sintax allows any js expression to be evaluated
+  title: string = 'Task tracker';
 }
