3654603aeecc33b6206491b1601172ec47920a86
fabriccio27 <fabricio_inc27@hotmail.com>
2023-11-03 16:40:18 -0300

    Modify service to represent how it would work work with an Observable

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
+/* in order to use a service you have to add it as a provider to the constructor */
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    // this is the way to tell that whenever the component calls getTasks(), the result of that will be set to this.tasks
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+    /*
+    The subscribe method is used to subscribe to the Observable.
+    This sets up a subscription, and the provided function (tasks => this.tasks = tasks)
+    is the callback that executes when the Observable emits a value (in this case, an array of Task objects). */
+  }
+
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable,of } from 'rxjs';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+  getTasks(): Observable<Task[]> {
+    // we need to use of from rxjs to turn the TASKS array into an observable
+    const tasks = of(TASKS);
+    return tasks;
+  }
 }
