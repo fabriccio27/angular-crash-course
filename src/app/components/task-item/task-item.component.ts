e438c0eb5e8f18770dbfd39896e2075d0892bd43
fabriccio27 <fabricio_inc27@hotmail.com>
2023-11-03 15:38:46 -0300

    Add styling to task-item component

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
-export class TaskItemComponent {
-
+export class TaskItemComponent implements OnInit {
+/* the exclamation mark allows having the variable not initialized in the constructor */
+@Input() task!: Task;
+constructor() { }
+ngOnInit(): void {}
 }
