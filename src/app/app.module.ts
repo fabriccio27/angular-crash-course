2dc1583277c38e064ad6b176d0e397df08b28129
fabriccio27 <fabricio_inc27@hotmail.com>
2023-11-03 16:57:28 -0300

    Add HttpClientModule to global imports and replace hardcoded tasks with actual call to json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -21,7 +22,8 @@ and if it didn't you have do declare them here*/
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable,of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+// angular being a framework uses an http client to make requests to a server, not fetch or axios like in react
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
+  // by adding http as a provider, we got access to all of the methods of HttpClient
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
   getTasks(): Observable<Task[]> {
     // we need to use of from rxjs to turn the TASKS array into an observable
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
