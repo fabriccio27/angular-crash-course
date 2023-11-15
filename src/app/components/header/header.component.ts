d3b0d2548c6caf54689956faf73855297cd6031a
fabriccio27 <fabricio_inc27@hotmail.com>
2023-11-15 10:23:47 -0300

    Conditionally display html based on route the component is in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,11 +15,14 @@ export class HeaderComponent {
   showAddTask!: boolean;
   subscription!: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
   toggleAddTask (): void {
     this.uiService.toggleAddTask();
+  }
+  hasRoute(route: string): boolean {
+    return this.router.url === route;
   }
 }
