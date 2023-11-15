6c982194aaf260bf025167ba2ea5372c3e95e4d8
fabriccio27 <fabricio_inc27@hotmail.com>
2023-11-15 09:44:50 -0300

    Add initial config for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,6 +12,10 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 
+
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+];
 /* everytime I generate a new component or install a package it gets "recognized here",
 and if it didn't you have do declare them here*/
 
@@ -28,8 +33,13 @@ and if it didn't you have do declare them here*/
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* RouterModule.forRoot() not only configures routes but also initializes the router at the application's root
+level and registers router services globally. It provides services like Router and ActivatedRoute, among others,
+that are essential for navigation and route handling throughout your application. */
